f5e9209d0fe2700e78ae4818867eb98e35f97e99
liampaddick <liampaddick@users.noreply.github.com>
2019-02-10 19:43:37 +0000

    Added classes for questions and answers as well as constructors
    Added classes for questions and answers as well as constructors to init objects of either of these classes. also added functions that will be used to create all of the question/answer objects.

--- TheoryTest/TheoryTest/Program.cs
+++ TheoryTest/TheoryTest/Program.cs
@@ -18,5 +18,49 @@ namespace TheoryTest
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+        static Question[] initQuestions()
+        {
+            Question[] tempQuestionArray = new Question[50];
+            return tempQuestionArray;
+        }
+
+        static Answer[] initAnswers()
+        {
+            Answer[] tempAnswerArray = new Answer[200];
+            return tempAnswerArray;
+        }
+    }
+
+    class Question
+    {
+        int localID;
+        string localQuestionText;
+        Answer[] localPossibleAnswerArray = new Answer[4];
+
+        public Question(int id, string questionText, Answer answer1, Answer answer2, Answer answer3, Answer answer4)
+        {
+            localID = id;
+            localQuestionText = questionText;
+            localPossibleAnswerArray[0] = answer1;
+            localPossibleAnswerArray[1] = answer2;
+            localPossibleAnswerArray[2] = answer3;
+            localPossibleAnswerArray[3] = answer4;
+        }
+    }
+
+    class Answer
+    {
+        int localLinkedQuestionID;
+        int localAnswerID;
+        string localAnswerText;
+        bool localCorrectAnswer;
+
+        public Answer(int linkedQuestionID, int answerID, string answerText, bool correctAnswer)
+        {
+            localLinkedQuestionID = linkedQuestionID;
+            localAnswerID = answerID;
+            localAnswerText = answerText;
+            localCorrectAnswer = correctAnswer;
+        }
     }
 }
