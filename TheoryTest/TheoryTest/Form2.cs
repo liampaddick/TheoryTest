a0cce31867a98bc295ae6e5dc115d5ad4120c382
liampaddick <liampaddick@users.noreply.github.com>
2019-02-19 22:35:34 +0000

    Added dynamic button creation
    Buttons are created depending on the amount of results passed from the first form.

--- TheoryTest/TheoryTest/Form2.cs
+++ TheoryTest/TheoryTest/Form2.cs
@@ -12,11 +12,23 @@ namespace TheoryTest
 {
     public partial class Form2 : Form
     {
-        List<bool> localResults = new List<bool> { };
+        List<bool> localResultsList = new List<bool> { };
+        List<Button> createdButtonsList = new List<Button> { };
         public Form2(List<bool> passedResults)
         {
             InitializeComponent();
-            localResults = passedResults;
+            localResultsList = passedResults;
+
+            for (int i = 0; i < localResultsList.Count(); i++)
+            {
+                Button tempButton = new Button();
+                tempButton.Name = "Button" + i.ToString();
+                tempButton.Text = tempButton.Name;
+                tempButton.Size = new Size(20, 20);
+                tempButton.Location = new Point(50 + (50 * i), 50);
+                createdButtonsList.Add(tempButton);
+                Controls.Add(tempButton);
+            }
         }
     }
 }
