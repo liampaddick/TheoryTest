3bdd21ef3b98fa6d697c243a54e989b71a8a20f0
liampaddick <liampaddick@users.noreply.github.com>
2019-02-19 22:42:39 +0000

    Tested with 10 buttons and made them move down a row after 5 buttons have beencreated
    Tested with 10 buttons and made them move down a row after 5 buttons have been created.

--- TheoryTest/TheoryTest/Form2.cs
+++ TheoryTest/TheoryTest/Form2.cs
@@ -12,20 +12,34 @@ namespace TheoryTest
 {
     public partial class Form2 : Form
     {
-        List<bool> localResultsList = new List<bool> { };
-        List<Button> createdButtonsList = new List<Button> { };
         public Form2(List<bool> passedResults)
         {
             InitializeComponent();
+            List<bool> localResultsList = new List<bool> { };
+            List<Button> createdButtonsList = new List<Button> { };
             localResultsList = passedResults;
+            int startingXCo = 50;
+            int startingYCo = 50;
+            int xCo = 50;
+            int yCo = 50;
+            int multiplier = 0;
 
-            for (int i = 0; i < localResultsList.Count(); i++)
+            for (int i = 0; i < localResultsList.Count() + 5; i++)
             {
                 Button tempButton = new Button();
                 tempButton.Name = "Button" + i.ToString();
                 tempButton.Text = tempButton.Name;
                 tempButton.Size = new Size(20, 20);
-                tempButton.Location = new Point(50 + (50 * i), 50);
+
+                if (i % 5 == 0)
+                {
+                    yCo = yCo + 50;
+                    xCo = startingXCo;
+                    multiplier = 0;
+                }
+
+                tempButton.Location = new Point(xCo + (xCo * multiplier), yCo);
+                multiplier++;
                 createdButtonsList.Add(tempButton);
                 Controls.Add(tempButton);
             }
